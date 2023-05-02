fd79a7e370dc1fab3bceed7484a1117ebe55fc5d
Angel <angel-ortega27@hotmail.com>
2023-05-01 17:22:18 -0700

    added clear functionality to application

--- js/script.js
+++ js/script.js
@@ -7,9 +7,8 @@ window.addEventListener("load", () => {
   let count = 0;
   let start = [];
   let end = [];
-  let knight = document.createElement("img");
-  knight.src = "./img/chess.png";
-  knight.classList.add("knight");
+  let squares = null;
+  let knight;
 
   //dynamially grabs ID's of HTML elements
   const $ = (id) => {
@@ -21,6 +20,7 @@ window.addEventListener("load", () => {
   let placeKnightBtn = $("start");
   let endKnightBtn = $("end");
   let travailBtn = $("travail");
+  let clear = $("clear");
   let board = modules.buildBoard();
 
   //uses eventListner to let clicked square return a pair of cordinates;
@@ -35,22 +35,27 @@ window.addEventListener("load", () => {
     globalCords = cords;
     if (count % 2 !== 0) {
       s(globalCords);
+      //create knight image
+      knight = document.createElement("img");
+      knight.src = "./img/chess.png";
+      knight.classList.add("knight");
+      //append knight image to choose square
+      chessSquare.classList.add("knight");
       chessSquare.appendChild(knight);
     } else {
       t(globalCords);
+      //add red sqaure to end piece
       chessSquare.classList.add("start");
     }
   };
-
+  //returns start and end points of respective function
   const s = (x) => {
     start = x;
   };
   const t = (y) => {
     end = y;
   };
-
   const addCords = () => {
-    let squares = null;
     for (let i = 0; i < rows.length; i++) {
       squares = rows[i].getElementsByTagName("td");
       for (let n = 0; n < squares.length; n++) {
@@ -60,16 +65,39 @@ window.addEventListener("load", () => {
         squares[n].addEventListener("click", returnCords);
       }
     }
-    return false;
+    placeKnightBtn.disabled = true;
   };
-
+  const clearBoard = () => {
+    globalCords = [];
+    count = 0;
+    start = [];
+    end = [];
+    knight.parentNode.removeChild(knight);
+    for (let i = 0; i < rows.length; i++) {
+      squares = rows[i].getElementsByTagName("td");
+      for (let n = 0; n < squares.length; n++) {
+        if (squares[n].classList.contains("start")) {
+          squares[n].classList.remove("start");
+        }
+      }
+    }
+  };
   placeKnightBtn.addEventListener("click", () => {
     addCords();
   });
   endKnightBtn.addEventListener("click", () => {
+    endKnightBtn.disabled = true;
     addCords();
   });
   travailBtn.addEventListener("click", () => {
     let path = modules.knightMoves(start, end);
+    path.forEach((element) => {
+      console.log(element);
+    });
+    placeKnightBtn.disabled = false;
+    endKnightBtn.disabled = false;
+  });
+  clear.addEventListener("click", () => {
+    clearBoard();
   });
 });
