424f1430751352398d8629ce0db67eb70f4b0ac8
Angel <angel-ortega27@hotmail.com>
2023-05-01 18:29:24 -0700

    added randomKnight function to application

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ window.addEventListener("load", () => {
   let end = [];
   let squares = null;
   let knight;
-
   //dynamially grabs ID's of HTML elements
   const $ = (id) => {
     return document.getElementById(id);
@@ -19,8 +18,11 @@ window.addEventListener("load", () => {
   let rows = gameBoard.getElementsByTagName("tr");
   let placeKnightBtn = $("start");
   let endKnightBtn = $("end");
-  let travailBtn = $("travail");
+  let randomKnight = $("random");
+  let travailBtn = $("travail"); //disable to prevent user from inputing with null array
+  travailBtn.disabled = true;
   let clear = $("clear");
+  clear.disabled = true; //disable to prevent user from clearing board mid move
   let board = modules.buildBoard();
 
   //uses eventListner to let clicked square return a pair of cordinates;
@@ -65,7 +67,6 @@ window.addEventListener("load", () => {
         squares[n].addEventListener("click", returnCords);
       }
     }
-    placeKnightBtn.disabled = true;
   };
   const clearBoard = () => {
     globalCords = [];
@@ -82,11 +83,43 @@ window.addEventListener("load", () => {
       }
     }
   };
+
+  const createRandomKnight = () => {
+    let x = Math.floor(Math.random() * 8);
+    let y = Math.floor(Math.random() * 8);
+
+    count++;
+    let cords = [x, y];
+    globalCords = cords;
+    console.log(globalCords);
+    s(globalCords);
+    placeKnightBtn.disabled = true;
+
+    for (let i = 0; i < rows.length; i++) {
+      squares = rows[i].getElementsByTagName("td");
+      for (let n = 0; n < squares.length; n++) {
+        if (
+          squares[n].dataset["xcord"] === `${x}` &&
+          squares[n].dataset["ycord"] === `${y}`
+        ) {
+          knight = document.createElement("img");
+          knight.src = "./img/chess.png";
+          knight.classList.add("knight");
+          //append knight image to choose square
+          squares[n].classList.add("knight");
+          squares[n].appendChild(knight);
+        }
+      }
+    }
+  };
+
   placeKnightBtn.addEventListener("click", () => {
     addCords();
+    placeKnightBtn.disabled = true;
   });
   endKnightBtn.addEventListener("click", () => {
     endKnightBtn.disabled = true;
+    travailBtn.disabled = false;
     addCords();
   });
   travailBtn.addEventListener("click", () => {
@@ -96,8 +129,22 @@ window.addEventListener("load", () => {
     });
     placeKnightBtn.disabled = false;
     endKnightBtn.disabled = false;
+    clear.disabled = false;
+    travailBtn.disabled = true;
+  });
+  randomKnight.addEventListener("click", () => {
+    for (let i = 0; i < rows.length; i++) {
+      squares = rows[i].getElementsByTagName("td");
+      for (let n = 0; n < squares.length; n++) {
+        //attaches cordinate values to each chessboard piece
+        squares[n].dataset.xcord = i;
+        squares[n].dataset.ycord = n;
+      }
+    }
+    createRandomKnight();
   });
   clear.addEventListener("click", () => {
     clearBoard();
+    clear.disabled = true;
   });
 });
